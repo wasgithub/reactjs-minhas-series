944c99b29d06aa9ffbc3982ceca159821a0cf91f
Washington Alexandre da Silva <washington.silva@SPOMM010114173.local>
2018-11-21 12:33:45 -0200

    subindo projeto

--- src/App.js
+++ src/App.js
@@ -1,28 +1,54 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import {
+  BrowserRouter as Router,
+  Route, Link
+} from 'react-router-dom'
+
+import api from './Api'
+import Home from './Home'
+import NewSeries from './NewSeries'
+
+const About = () => <section className="intro-section">Sobre</section>
 
 class App extends Component {
+  
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+          <nav className="navbar navbar-default navbar-fixed-top" role="navigation">
+            <div className="container">
+              <div className="navbar-header page-scroll">
+                <a className="navbar-brand page-scroll" href="#page-top">
+                    <img src="images/logo.png" height="30" />
+                </a>
+              </div>
+
+              <div className="collapse navbar-collapse navbar-ex1-collapse">
+                <ul className="nav navbar-nav">
+                  <li>
+                    <Link to="/">Home</Link>
+                  </li>
+                  <li>
+                    <Link to="/new">Nova série</Link>
+                  </li>
+                  <li>
+                    <Link to="/about">Sobre</Link>
+                  </li>
+
+                </ul>
+              </div>
+
+            </div>
+          </nav>
+          <Route exact path='/' component={Home}/>
+          <Route exact path='/new' component={NewSeries}/>
+          <Route exact path='/about' component={About}/>
+
+        </div>
+      </Router>
     );
   }
 }
 
-export default App;
+export default App
---
+++ src/NewSeries.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class NewSeries extends Component {
+
+  render() {
+    return (
+      <section className="intro-section">Nova série</section>
+    )
+  }
+}
+
+export default NewSeries
